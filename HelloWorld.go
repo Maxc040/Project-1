3421ab6b3946b6bb28b2e49ce0289786d6f2fd75
Maxc040 <Max.cornelissen@student.fontys.nl>
2023-02-09 13:12:05 +0100

    Versie 1.1 uitleg toegevoegd

--- HelloWorld.go
+++ HelloWorld.go
@@ -1,9 +1,17 @@
+//Maak een applicatie in Golang
+//Welke een welkomstbericht (bijv.: Welkom bij Fonteyn Vakantieparken) naar de console schrijft.
+
 package main
 
 import "fmt"
 
+//een pakket dat is geïmporteerd, hier zitten functies in etc.
+
 func main() {
 
-	fmt.Println("!... Hello World ...!")
+	fmt.Println("Welkom bij Fonteyn Vakantieparken!")
 
 }
+
+//printin word gebruikt om het bericht "Welkom bij Fonteyn Vakantieparken!" te printen op de console.
+//main is de funtie die automatisch word uitgevoerd wanneer ik het programma run.
